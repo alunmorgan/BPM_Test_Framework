f104b4589640d42e4e729889d25705752a2ae609
Alun Morgan <alun.morgan@diamond.ac.uk>
2018-03-13 17:09:18 +0000

    Internal attenuator check now almost works. Some work on the graph needed.

--- Tests/ADC_int_atten_sweep.py
+++ Tests/ADC_int_atten_sweep.py
@@ -9,12 +9,14 @@ from math import floor
 from scipy.io import savemat
 
 
-def adc_int_atten_sweep_test(rf_object,
+def adc_int_atten_sweep_test(
+                             test_system_object,
+                             rf_object,
                              bpm_object,
                              prog_atten_object,
                              frequency,
                              power_level=-20,
-                             attenuation_levels=np.arange(6, 35, 3),
+                             attenuation_levels=np.arange(0, 62, 2),
                              settling_time=1,
                              report_object=None,
                              sub_directory=""):
@@ -23,6 +25,7 @@ def adc_int_atten_sweep_test(rf_object,
     The RF signal is turned off, and then different parameters are measured from the BPM.
 
     Args:
+        test_system_object (System Obj): Object capturing the system losses and hardware ids.
         rf_object (RFSignalGenerator Obj): Object to interface with the RF hardware.
         bpm_object (BPMDevice Obj): Object to interface with the BPM hardware.
         prog_atten_object (Prog_Atten Obj): Object to interface with programmable attenuator hardware
@@ -38,73 +41,16 @@ def adc_int_atten_sweep_test(rf_object,
         float array: X ADC data read from the BPM
         float array: Y ADC data read from the BPM
     """
-
-    # Formats the test name and tells the user the test has started
-    test_name = __name__
-    test_name = test_name.rsplit("Tests.")[1]
-    test_name = test_name.replace("_", " ")
-    print("Starting test \"" + test_name + "\"")
-
-    # Initial setup of the RF system.
-    rf_object.turn_off_RF()
-    rf_object.set_frequency(frequency)
-    # Forcing to be an int as decimal points will cause the command sent to fail
-    rf_object.set_output_power(int(power_level))
-    prog_atten_object.set_global_attenuation(0)
-    rf_object.turn_on_RF()
-    bpm_object.set_attenuation(6)
+    test_name = test_system_object.test_initialisation(__name__, rf_object, prog_atten_object, frequency, power_level)
+    bpm_object.set_internal_state(agc='AGC off', delta=0, offset=0, switches='Manual',
+                                  switch_state=15, attenuation=0, dsc='Unity gains',
+                                  ft_state='Enabled')
+    # Wait for system to settle
     time.sleep(settling_time)
-    adc_n_bits = 16
-
-    adc_max_counts = np.power(2, adc_n_bits)
-    adc_step = adc_max_counts / adc_n_bits
-    bin_edges = range(adc_n_bits + 1)
-
-    input_power = []
-    output_power = []
-    data1 = []
-    data2 = []
-    data3 = []
-    data4 = []
-    times = []
-    graph_legend = []
-    #  Gradually reducing the power level
-    for index in attenuation_levels:
-        # Set attenuator value to give desired power level.
-        bpm_object.set_attenuation(index)
-        time.sleep(settling_time)  # Wait for signal to settle
-        # Gets 1024 samples for each ADC.
-        time_tmp, data1_tmp, data2_tmp, data3_tmp, data4_tmp = bpm_object.get_adc_data()  # record data
-        times.append(time_tmp)
-        #  Identifying which bit bin a value is in.
-        data1_tmp[:] = [x / adc_step for x in data1_tmp]
-        data2_tmp[:] = [x / adc_step for x in data2_tmp]
-        data3_tmp[:] = [x / adc_step for x in data3_tmp]
-        data4_tmp[:] = [x / adc_step for x in data4_tmp]
-        data1.append(data1_tmp)
-        data2.append(data2_tmp)
-        data3.append(data3_tmp)
-        data4.append(data4_tmp)
-        output_power.append(index)
-        input_power.append(bpm_object.get_input_power())
-        graph_legend.append(str(index))
-
-    # turn off the RF
-    rf_object.turn_off_RF()
-    # Get the plot values in a format that is easy to iterate
-    format_plot = []  # x axis, y axis, x axis title, y axis title, title of file, caption
-    format_plot.append(((times, data1), ('bit number', 'counts',
-                                         "ADC1_histogram_varying_internal_attenuation.pdf", graph_legend)))
-    format_plot.append(((times, data2), ('bit number', 'counts',
-                                         "ADC2_histogram_varying_internal_attenuation.pdf", graph_legend)))
-    format_plot.append(((times, data3), ('bit number', 'counts',
-                                         "ADC3_histogram_varying_internal_attenuation.pdf", graph_legend)))
-    format_plot.append(((times, data4), ('bit number', 'counts',
-                                         "ADC4_histogram_varying_internal_attenuation.pdf", graph_legend)))
 
     if report_object is not None:
-        intro_text = r"""Excites with a sine wave and then gets the ADC data from each channel. 
-        Plots the histogram to that any missing bits can be identified.
+        intro_text = r"""Excites with different signal levels. Gets the ADC data from each channel at each signal level. 
+        Plots the curve.
 
         The RF signal is a sine wave.  \\~\\
         """
@@ -119,25 +65,63 @@ def adc_int_atten_sweep_test(rf_object,
         # add the test details to the report
         report_object.setup_test(test_name, intro_text, device_names, parameter_names)
 
-    # plot all of the graphs
+    data = np.empty((len(attenuation_levels), 1024, bpm_object.num_adcs))
+    # data_std = np.empty((len(attenuation_levels), bpm_object.num_adcs))
+    # data1_mean = []
+    # data2_mean = []
+    # data3_mean = []
+    # data4_mean = []
+    # data1_std = []
+    # data2_std = []
+    # data3_std = []
+    # data4_std = []
+    # times = []
+    # graph_legend = []
+    #  Gradually reducing the power level
+    level_ind = 0
+    for index in attenuation_levels:
+        # Set attenuator value to give desired power level.
+        bpm_object.set_attenuation(index)
+        time.sleep(settling_time)  # Wait for signal to settle
+        # Gets 1024 samples for each ADC.
+        time_tmp, data[level_ind, :, 0], \
+                  data[level_ind, :, 1], \
+                  data[level_ind, :, 2], \
+                  data[level_ind, :, 3] = bpm_object.get_adc_data()  # record data
 
-    for index in format_plot:
-        #if len(index[1]) == 4:
-        plt.hist(np.transpose(np.array(index[0][1])), bins=bin_edges, label=index[1][3])
-        #else:
-        #    plt.hist(np.transpose(np.array(index[0][1])), bins=bin_edges)
-        plt.legend()
-        plt.xlabel(index[1][0])
-        plt.ylabel(index[1][1])
-        if report_object is None:
-            # If no report is entered as an input to the test, simply display the results
-            plt.show()
-        else:
-            plt.savefig(''.join((sub_directory, index[1][2])))
-            report_object.add_figure_to_test(image_name=''.join((sub_directory, index[1][2])), caption=index[1][2])
-        plt.cla()  # Clear axis
-        plt.clf()  # Clear figure
+    # turn off the RF
+    rf_object.turn_off_RF()
+
+    data_mean = np.mean(data, axis=1)
+    data_std = np.std(data, axis=1)
 
     savemat(sub_directory + "ADC_int_atten_sweep_data" + ".mat",
-            {'data': data,
+            {'data_mean': data_mean,
              'data_std': data_std})
+
+    # Get the plot values in a format that is easy to iterate
+    format_plot = []  # x axis, y axis, x axis title, y axis title, title of file, caption
+    for hw in range(bpm_object.num_adcs):
+        format_plot.append(((attenuation_levels, data_mean[:, hw], data_std[:, hw]), ('attenuation', 'counts',
+                            "ADC_varying_internal_attenuation.pdf", ' '.join(('ADC', str(hw))))))
+
+    # plot all of the graphs
+    cols = ['k', 'r', 'g', 'b']
+    cols_ind = 0
+    for index in format_plot:
+        plt.plot(index[0][0], index[0][1], color=cols[cols_ind], label=index[1][3])
+        cols_ind = cols_ind + 1
+    plt.legend(loc='upper right')
+    plt.xlabel(format_plot[0][1][0])
+    plt.ylabel(format_plot[0][1][1])
+    plt.xlim(attenuation_levels[0], attenuation_levels[-1])
+    if report_object is None:
+        # If no report is entered as an input to the test, simply display the results
+        plt.show()
+    else:
+        plt.savefig(''.join((sub_directory, format_plot[0][1][2])))
+        report_object.add_figure_to_test(image_name=''.join((sub_directory, format_plot[0][1][2])),
+                                         caption='Internal attenuator check')
+    plt.cla()  # Clear axis
+    plt.clf()  # Clear figure
+
