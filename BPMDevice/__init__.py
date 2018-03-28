1904d750c9c04d589ab37289f72e90fdf0e3872b
Alun Morgan <alun.morgan@diamond.ac.uk>
2018-03-28 11:40:17 +0100

    Adding the Brilliance BPM to the init.

--- BPMDevice/__init__.py
+++ BPMDevice/__init__.py
@@ -1,5 +1,6 @@
 from Simulated_BPMDevice import *
 from ElectronBPMDevice import *
+from BrillianceBPMDevice import *
 from SparkER_SCPI_BPMDevice import *
 from SparkERXR_EPICS_BPMDevice import *
 from BPM_helper_functions import *
