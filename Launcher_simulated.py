603d313465b15e08046a2561e0871674f3d5867c
Alun Morgan <alun.morgan@diamond.ac.uk>
2019-01-25 10:10:11 +0000

    Updated the simulated launcher to use json.

--- Launcher_simulated.py
+++ Launcher_simulated.py
@@ -1,117 +1,105 @@
 import sys
 import os
 import time
-import RFSignalGenerators
-import BPMDevice
-import Gate_Source
-import ProgrammableAttenuator
+from pkg_resources import require
+require('numpy == 1.11.1')
+require('scipy == 0.19.1')
+require("matplotlib")
+import numpy as np
+import Test_system_common
 import Latex_Report
 import Tests
-import numpy as np
+import json
+# from scipy.io import savemat
 
+SYS = Test_system_common.TestSystem(rf_hw='Simulated', bpm_hw='Simulated', atten_hw='Simulated')
 
-RF = RFSignalGenerators.Simulated_RFSigGen(limit=-40, noise_mag=1)
-GS = Gate_Source.Simulated_GateSource()
-ProgAtten = ProgrammableAttenuator.Simulated_Prog_Atten()
-BPM = BPMDevice.SimulatedBPMDevice(rf_sim=RF, gatesim=GS, progatten=ProgAtten, noise_mag=1)
-
-root_path = '/'.join((sys.argv[1], BPM.macaddress.replace(':', '-'), time.strftime("%d-%m-%Y_T_%H-%M")))
+root_path = '/'.join((sys.argv[1], SYS.BPM.mac_address.replace(':', '-'), time.strftime("%d-%m-%Y_T_%H-%M")))
 print root_path
 if not os.path.exists(root_path):
     os.makedirs(root_path)
 
-report = Latex_Report.Tex_Report('/'.join((root_path, "BPMTestReport")), BPM.macaddress)
+# report = Latex_Report.Tex_Report('/'.join((root_path, "BPMTestReport")), SYS.BPM.macaddress)
 
 dls_RF_frequency = 499.6817682
 dls_bunch = 1. / (dls_RF_frequency / 936.)  # us
 subdirectory = ''.join((root_path, '/'))
 settling_time = 0
 
-ProgAtten.set_global_attenuation(0)
 
-Tests.beam_position_equidistant_grid_raster_scan_test(
-    rf_object=RF,
-    bpm_object=BPM,
-    prog_atten_object=ProgAtten,
-    rf_frequency=dls_RF_frequency,
-    rf_power=-40,
-    nominal_attenuation=10,
-    x_points=3,
-    y_points=3,
-    settling_time=settling_time,
-    report_object=report,
-    sub_directory=subdirectory)
+data_out = {'epics_id': SYS.BPM.epics_id,
+            'mac_address': SYS.BPM.mac_address,
+            'first_turn': SYS.BPM.ft,
+            'agc': SYS.BPM.agc,
+            'delta': SYS.BPM.delta,
+            'switches': SYS.BPM.switches,
+            'switch_val': SYS.BPM.switch_val,
+            'attenuation': SYS.BPM.attn,
+            'dsc': SYS.BPM.dsc,
+            'kx': SYS.BPM.kx,
+            'ky': SYS.BPM.ky}
 
-Tests.beam_position_attenuation_permutation_test(
-    rf_object=RF,
-    bpm_object=BPM,
-    prog_atten_object=ProgAtten,
-    rf_frequency=dls_RF_frequency,
-    rf_power=-40,
-    attenuator_max=50,
-    attenuator_min=10,
-    attenuator_steps=2,
-    settling_time=settling_time,
-    report_object=report,
-    sub_directory=subdirectory)
+with open(subdirectory + "initial_BPM_state.json", 'w') as write_file:
+    json.dump(data_out, write_file)
 
-Tests.beam_power_dependence(
-    rf_object=RF,
-    bpm_object=BPM,
-    prog_atten_object=ProgAtten,
-    frequency=dls_RF_frequency,
-    power_levels=np.arange(-40, -100, -10),
-    settling_time=settling_time,
-    report_object=report,
-    sub_directory=subdirectory)
+Tests.beam_position_equidistant_grid_raster_scan_test(test_system_object=SYS,
+                                                      rf_frequency=dls_RF_frequency,
+                                                      power_level=-40,
+                                                      nominal_attenuation=10,
+                                                      x_points=3,
+                                                      y_points=3,
+                                                      settling_time=settling_time,
+                                                      sub_directory=subdirectory
+                                                      )
 
-Tests.fixed_voltage_amplitude_fill_pattern_test(
-    rf_object=RF,
-    bpm_object=BPM,
-    prog_atten_object=ProgAtten,
-    gate_source_object=GS,
-    frequency=dls_RF_frequency,
-    max_power=-40,
-    duty_cycles=np.arange(1, 0, -0.1),
-    pulse_period=dls_bunch,
-    settling_time=settling_time,
-    report_object=report,
-    sub_directory=subdirectory)
+Tests.beam_power_dependence(test_system_object=SYS,
+                            frequency=dls_RF_frequency,
+                            power_levels=np.arange(-40, -100, -10),
+                            settling_time=settling_time,
+                            sub_directory=subdirectory
+                            )
 
-Tests.scaled_voltage_amplitude_fill_pattern_test(
-    rf_object=RF,
-    bpm_object=BPM,
-    prog_atten_object=ProgAtten,
-    gate_source_object=GS,
-    frequency=dls_RF_frequency,
-    max_power=-40,
-    duty_cycles=np.arange(1, 0, -0.1),
-    pulse_period=dls_bunch,
-    settling_time=settling_time,
-    report_object=report,
-    sub_directory=subdirectory)
+# Tests.fixed_voltage_amplitude_fill_pattern_test(test_system_object=SYS,
+#                                                 frequency=dls_RF_frequency,
+#                                                 max_power=-40,
+#                                                 duty_cycles=np.arange(1, 0, -0.1),
+#                                                 pulse_period=dls_bunch,
+#                                                 settling_time=settling_time,
+#                                                 sub_directory=subdirectory
+#                                                 )
+#
+# Tests.bunch_train_length_dependency_test(test_system_object=SYS,
+#                                          frequency=dls_RF_frequency,
+#                                          max_power=-40,
+#                                          duty_cycles=np.arange(1, 0, -0.1),
+#                                          pulse_period=dls_bunch,
+#                                          settling_time=settling_time,
+#                                          sub_directory=subdirectory
+#                                          )
 
-ProgAtten.set_global_attenuation(0)
-
-Tests.noise_test(rf_object=RF,
-                 bpm_object=BPM,
+Tests.noise_test(test_system_object=SYS,
                  frequency=dls_RF_frequency,
-                 start_power=-100,
-                 end_power=-40,
+                 power_levels=np.arange(-20, -50, -5),
                  samples=10,
                  settling_time=settling_time,
-                 report_object=report,
-                 sub_directory=subdirectory)
+                 sub_directory=subdirectory
+                 )
 
-Tests.adc_test(rf_object=RF,
-               bpm_object=BPM,
-               prog_atten_object=ProgAtten,
+Tests.adc_test(test_system_object=SYS,
                frequency=1,
-               samples=1000,
-               power_levels=(-40., -50.),
-               settling_time=1,
-               report_object=report,
-               sub_directory=subdirectory)
+               power_level=-40,
+               settling_time=10,
+               sub_directory=subdirectory
+               )
 
-report.create_report()
+Tests.adc_int_atten_sweep_test(test_system_object=SYS,
+                               frequency=dls_RF_frequency,
+                               power_level=-20,
+                               external_attenuation=60,
+                               attenuation_levels=np.arange(0, 62, 2),
+                               settling_time=settling_time,
+                               sub_directory=subdirectory)
+
+Latex_Report.assemble_report(subdirectory=subdirectory)
+# report.create_report()
 
